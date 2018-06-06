6946764c01697be64191a6fd177646cdc8d1ea61
Gothicdread <35009972+Gothicdread@users.noreply.github.com>
2018-06-06 14:04:23 +0100

    Add files via upload

---
+++ Assembly/Template.asm
@@ -0,0 +1,18 @@
+.8086
+.model	small
+.stack	2048
+
+dseg    segment para public 'data'
+ 	;Variaveis
+dseg    ends
+
+cseg	segment para public 'code'
+	assume  cs:cseg, ds:dseg
+main	proc
+	mov     ax, dseg
+	mov     ds, ax
+	; Código
+	
+main	endp
+cseg    ends
+end     main
