0d19b1e275a0007c3e4fea29cdf018e81699f065
Gothicdread <35009972+Gothicdread@users.noreply.github.com>
2018-06-06 19:36:08 +0100

    Update TPBG.asm

--- Assembly/TP Bruno Gabriel/TPBG.asm
+++ Assembly/TP Bruno Gabriel/TPBG.asm
@@ -9,35 +9,59 @@ dseg    segment para public 'data'
 
 	str_num db 5 dup(?),'$'
 	
-        linha		db	0	; Define o número da linha que está a ser desenhada
-        nlinhas		db	0
-	cor		db 	0
-	car		db	' '	
+    linha		db	0	; Define o número da linha que está a ser desenhada
+    nlinhas		db	0
+	tcor		db 	0
+	tcar		db	' '
+	
+	string	db	"Teste prático de T.I",0
+		cCar		db	32	; Guarda um caracter do Ecran 
+		cCor		db	7	; Guarda os atributos de cor do caracter
+		cCar2		db	32	; Guarda um caracter do Ecran 
+		cCor2		db	7	; Guarda os atributos de cor do caracter
+		POSy		db	5	; a linha pode ir de [1 .. 25]
+		POSx		db	10	; POSx pode ir [1..80]	
+		POSya		db	5	; Posição anterior de y
+		POSxa		db	10	; Posição anterior de x
 	
 dseg    ends
 
 cseg	segment para public 'code'
 	assume  cs:cseg, ds:dseg
-main	proc
 	;PREPARAÇÃO DO PROGRAMA
 	;MACROS
 	
 	goto_xy	macro		POSx,POSy
-		mov		ah,02h
-		mov		bh,0		; numero da página
-		mov		dl,POSx
-		mov		dh,POSy
-		int		10h
+		mov ah, 02h
+		mov bh, 0		; numero da página
+		mov dl, POSx
+		mov dh, POSy
+		int 10h
 	endm
+	
+LE_TECLA	PROC
+
+		mov ah, 08h
+		int 21h
+		mov ah, 0
+		cmp al, 0
+		jne SAI_TECLA
+		mov ah, 08h
+		int 21h
+		mov ah, 1
+SAI_TECLA:	RET
+
+LE_TECLA	endp
 
 	;PROGRAMA
+main	proc
 	
-	MOV	AX, dseg
-	MOV	DS, AX
+	mov AX, dseg
+	mov DS, AX
 	
 	mov ax, 0b800h
 	mov es, ax
-	mov dh, car
+	mov dh, tcar
 	mov dl, 0h
 	mov cx, 25*80
 	
@@ -48,176 +72,180 @@ limpa_ecra:
 	add si, 2
 	loop limpa_ecra
 
-	mov	cx,10		; Faz o ciclo 10 vezes
+	mov cx, 10		; Faz o ciclo 10 vezes
 ciclo4:
-		call	CalcAleat
-		pop	ax 		; vai buscar 'a pilha o número aleatório
+	call CalcAleat
+	pop ax 		; vai buscar 'a pilha o número aleatório
 
-		mov	dl,cl	
-		mov	dh,70
-		push	dx		; Passagem de parâmetros a impnum (posição do ecran)
-		push	ax		; Passagem de parâmetros a impnum (número a imprimir)
-		call	impnum		; imprime 10 aleatórios na parte direita do ecran
-		loop	ciclo4		; Ciclo de impressão dos números aleatórios
+	mov dl, cl	
+	mov dh, 70
+	push dx		; Passagem de parâmetros a impnum (posição do ecran)
+	push ax		; Passagem de parâmetros a impnum (número a imprimir)
+	call impnum		; imprime 10 aleatórios na parte direita do ecran
+	loop ciclo4		; Ciclo de impressão dos números aleatórios
 		
-		mov   	ax, 0b800h	; Segmento de memória de vídeo onde vai ser desenhado o tabuleiro
-		mov   	es, ax	
-		mov	linha, 	8	; O Tabuleiro vai começar a ser desenhado na linha 8 
-		mov	nlinhas, 6	; O Tabuleiro vai ter 6 linhas
+	mov ax, 0b800h	; Segmento de memória de vídeo onde vai ser desenhado o tabuleiro
+	mov es, ax	
+	mov linha, 	8	; O Tabuleiro vai começar a ser desenhado na linha 8 
+	mov nlinhas, 6	; O Tabuleiro vai ter 6 linhas
 		
-ciclo2:		mov	al, 160		
-		mov	ah, linha
-		mul	ah
-		add	ax, 60
-		mov 	bx, ax		; Determina Endereço onde começa a "linha". bx = 160*linha + 60
+ciclo2:	
+	mov al, 160		
+	mov ah, linha
+	mul ah
+	add ax, 60
+	mov bx, ax		; Determina Endereço onde começa a "linha". bx = 160*linha + 60
 
-		mov	cx, 9		; São 9 colunas 
+	mov cx, 9		; São 9 colunas 
+	
 ciclo:  	
-		mov 	dh,	car	; vai imprimir o caracter "SPACE"
-		mov	es:[bx],dh	;
+	mov dh,	tcar	; vai imprimir o caracter "SPACE"
+	mov es:[bx], dh	;
 	
 novacor:	
-		call	CalcAleat	; Calcula próximo aleatório que é colocado na pilha 
-		pop	ax ; 		; Vai buscar 'a pilha o número aleatório
-		and 	al,01110000b	; posição do ecran com cor de fundo aleatório e caracter a preto
-		cmp	al, 0		; Se o fundo de ecran é preto
-		je	novacor		; vai buscar outra cor 
+	call CalcAleat	; Calcula próximo aleatório que é colocado na pilha 
+	pop ax ; 		; Vai buscar 'a pilha o número aleatório
+	and al, 01110000b	; posição do ecran com cor de fundo aleatório e caracter a preto
+	cmp al, 0		; Se o fundo de ecran é preto
+	je novacor		; vai buscar outra cor 
 
-		mov 	dh,	   car	; Repete mais uma vez porque cada peça do tabuleiro ocupa dois carecteres de ecran
-		mov	es:[bx],   dh		
-		mov	es:[bx+1], al	; Coloca as características de cor da posição atual 
-		inc	bx		
-		inc	bx		; próxima posição e ecran dois bytes à frente 
+	mov dh, tcar	; Repete mais uma vez porque cada peça do tabuleiro ocupa dois carecteres de ecran
+	mov es:[bx],   dh		
+	mov es:[bx+1], al	; Coloca as características de cor da posição atual 
+	inc bx		
+	inc bx		; próxima posição e ecran dois bytes à frente 
 
-		mov 	dh,	   car	; Repete mais uma vez porque cada peça do tabuleiro ocupa dois carecteres de ecran
-		mov	es:[bx],   dh
-		mov	es:[bx+1], al
-		inc	bx
-		inc	bx
+	mov dh, tcar	; Repete mais uma vez porque cada peça do tabuleiro ocupa dois carecteres de ecran
+	mov es:[bx], dh
+	mov es:[bx+1], al
+	inc bx
+	inc bx
 		
-		mov	di,1 ;delay de 1 centesimo de segundo
-		call	delay
-		loop	ciclo		; continua até fazer as 9 colunas que correspondem a uma liha completa
+	mov di, 1 ;delay de 1 centesimo de segundo
+	call delay
+	loop ciclo		; continua até fazer as 9 colunas que correspondem a uma liha completa
 		
-		inc	linha		; Vai desenhar a próxima linha
-		dec	nlinhas		; contador de linhas
-		mov	al, nlinhas
-		cmp	al, 0		; verifica se já desenhou todas as linhas 
-		jne	ciclo2		; se ainda há linhas a desenhar continua 
+	inc linha		; Vai desenhar a próxima linha
+	dec nlinhas		; contador de linhas
+	mov al, nlinhas
+	cmp al, 0		; verifica se já desenhou todas as linhas 
+	jne ciclo2		; se ainda há linhas a desenhar continua 
+	
 FIM:
-	MOV	AH,4Ch
-	INT	21h	
+	MOV AH, 4Ch
+	INT 21h	
 
 main	endp
 
 CalcAleat proc near
 
-	sub	sp,2		; 
-	push	bp
-	mov	bp,sp
-	push	ax
-	push	cx
-	push	dx	
-	mov	ax,[bp+4]
-	mov	[bp+2],ax
+	sub sp, 2
+	push bp
+	mov bp, sp
+	push ax
+	push cx
+	push dx	
+	mov ax, [bp+4]
+	mov [bp+2], ax
 
-	mov	ah,00h
-	int	1ah
+	mov ah, 00h
+	int 1ah
 
-	add	dx,ultimo_num_aleat	; vai buscar o aleatório anterior
-	add	cx,dx	
-	mov	ax,65521
-	push	dx
-	mul	cx			
-	pop	dx			 
-	xchg	dl,dh
-	add	dx,32749
-	add	dx,ax
+	add dx, ultimo_num_aleat	; vai buscar o aleatório anterior
+	add cx, dx	
+	mov ax, 65521
+	push dx
+	mul cx			
+	pop dx			 
+	xchg dl, dh
+	add dx, 32749
+	add dx, ax
 
-	mov	ultimo_num_aleat,dx	; guarda o novo numero aleatório  
+	mov ultimo_num_aleat, dx	; guarda o novo numero aleatório  
 
-	mov	[BP+4],dx		; o aleatório é passado por pilha
+	mov [BP+4], dx		; o aleatório é passado por pilha
 
-	pop	dx
-	pop	cx
-	pop	ax
-	pop	bp
+	pop dx
+	pop cx
+	pop ax
+	pop bp
 	ret
 CalcAleat endp
 
 impnum proc near
-	push	bp
-	mov	bp,sp
-	push	ax
-	push	bx
-	push	cx
-	push	dx
-	push	di
-	mov	ax,[bp+4] ;param3
-	lea	di,[str_num+5]
-	mov	cx,5
+	push bp
+	mov	bp, sp
+	push ax
+	push bx
+	push cx
+	push dx
+	push di
+	mov ax, [bp+4] ;param3
+	lea di, [str_num+5]
+	mov cx, 5
+	
 prox_dig:
-	xor	dx,dx
-	mov	bx,10
-	div	bx
-	add	dl,'0' ; dh e' sempre 0
-	dec	di
-	mov	[di],dl
-	loop	prox_dig
+	xor dx, dx
+	mov bx, 10
+	div bx
+	add dl, '0' ; dh é sempre 0
+	dec di
+	mov [di], dl
+	loop prox_dig
 
-	mov	ah,02h
-	mov	bh,00h
-	mov	dl,[bp+7] ;param1
-	mov	dh,[bp+6] ;param2
-	int	10h
-	mov	dx,di
-	mov	ah,09h
-	int	21h
-	pop	di
-	pop	dx
-	pop	cx
-	pop	bx
-	pop	ax
-	pop	bp
-	ret	4 ;limpa parametros (4 bytes) colocados na pilha
+	mov ah, 02h
+	mov bh, 00h
+	mov dl, [bp+7] ;param1
+	mov dh, [bp+6] ;param2
+	int 10h
+	mov dx, di
+	mov ah, 09h
+	int 21h
+	pop di
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	pop bp
+	ret 4 ;limpa parametros (4 bytes) colocados na pilha
 	
 impnum endp
 
 delay proc
 	pushf
-	push	ax
-	push	cx
-	push	dx
-	push	si
+	push ax
+	push cx
+	push dx
+	push si
 	
-	mov	ah,2Ch
-	int	21h
-	mov	al,100
-	mul	dh
-	xor	dh,dh
-	add	ax,dx
-	mov	si,ax
+	mov ah, 2Ch
+	int 21h
+	mov al, 100
+	mul dh
+	xor dh, dh
+	add ax, dx
+	mov si, ax
 
 
-ciclo:	mov	ah,2Ch
-	int	21h
-	mov	al,100
-	mul	dh
-	xor	dh,dh
-	add	ax,dx
+ciclo:	mov ah, 2Ch
+	int 21h
+	mov al, 100
+	mul dh
+	xor dh, dh
+	add ax, dx
 
-	cmp	ax,si 
-	jnb	naoajusta
-	add	ax,6000 ; 60 segundos
+	cmp ax, si 
+	jnb naoajusta
+	add ax, 6000 ; 60 segundos
 naoajusta:
-	sub	ax,si
-	cmp	ax,di
-	jb	ciclo
+	sub ax, si
+	cmp ax, di
+	jb ciclo
 
-	pop	si
-	pop	dx
-	pop	cx
-	pop	ax
+	pop si
+	pop dx
+	pop cx
+	pop ax
 	popf
 	ret
 delay endp
