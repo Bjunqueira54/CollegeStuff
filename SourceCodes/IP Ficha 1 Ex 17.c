c9c86f71d36bfc05f633641e073e8a5f7f234ee0
Gothicdread <35009972+Gothicdread@users.noreply.github.com>
2018-10-04 22:43:24 +0100

    Update IP Ficha 1 Ex 17.c

--- SourceCodes/IP Ficha 1 Ex 17.c
+++ SourceCodes/IP Ficha 1 Ex 17.c
@@ -1,5 +1,14 @@
+/*
+
+In this code I created an algorithm that fails or passes people in olympic competitions
+depending of their height and weight. Height has to be between 1.75 and 1.9 meters and weight
+is between 70 and 80 Kg
+
+*/
+
 #include <stdio.h>
-#define PESOMIN 70
+
+#define PESOMIN 70      //Multiple Defines. PESO = Weight and ALT = Height
 #define PESOMAX 80
 #define ALTMIN 1.75
 #define ALTMAX 1.9
@@ -8,26 +17,26 @@ void main()
 {
     float peso, altura;
 
-    printf("Qual o peso? ");
+    printf("Qual o peso? ");    //Asks for competitor's weight...
     scanf("%f", &peso);
 
-    printf("Qual a altura? ");
+    printf("Qual a altura? ");  //... and height
     scanf("%f", &altura);
 
-    if ((peso >= PESOMIN && peso <= PESOMAX) && (altura < ALTMIN || altura > ALTMAX))
-    {
-        printf("RECUSADO POR ALTURA!");
+    if ((peso >= PESOMIN && peso <= PESOMAX) && (altura < ALTMIN || altura > ALTMAX))   //ATTENTION TO PARENTHESIS HERE! Priority in coding is important
+    {                                                                                   //First you do what's inside () first. So this if condition is first testing if
+        printf("RECUSADO POR ALTURA!");   //It says here "Refused for Height"           //weight is within boundaries with "|| (or)", then checks for Height, and then checks both together
     }
     else if ((altura >= ALTMIN && altura <= ALTMAX) && (peso < PESOMIN || peso > PESOMAX))
     {
-        printf("RECUSADO POR PESO!");
+        printf("RECUSADO POR PESO!");   //It says here "Refused for Weight"
     }
     else if ((peso < PESOMIN || peso > PESOMAX) && (altura < ALTMIN || altura > ALTMAX))
     {
-        printf("TOTALMENTE RECUSADO!");
+        printf("TOTALMENTE RECUSADO!"); //This says "Totally Refused", so height and weight weren't in boundaries
     }
-    else
+    else    //In case none of the conditions above were met, that means the player is fit for competition
     {
-        printf("ACEITE!");
+        printf("ACEITE!");  //"ACCEPTED!"
     }
 }
