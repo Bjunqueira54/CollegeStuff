ea6673de14233c6e0900cfd455cd66d544765d52
Gothicdread <35009972+Gothicdread@users.noreply.github.com>
2018-10-04 17:45:34 +0100

    Update IP Ficha 1 Ex 10.c

--- SourceCodes/IP Ficha 1 Ex 10.c
+++ SourceCodes/IP Ficha 1 Ex 10.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+#include <stdio.h>  //included library. STandard Input Out
 
-void main()
+void main() //main function. VOID returns nothing
 {
-    int valor, horas, minutosIntermedio, minutos, segundos;
+    int valor, horas, minutosIntermedio, minutos, segundos; //multiple INTegers variables
 
-    printf("Introduza o valor total de segundos: ");
-    scanf("%i",&valor);
+    printf("Introduza o valor total de segundos: ");    //Asking for total amount of seconds. Example: 3600s  = 1 hour
+    scanf("%i",&valor); //scans the input for a %i and stores it into "valor"
 
-    horas=valor/3600;
-    minutosIntermedio=valor%3600;
-    minutos=minutosIntermedio/60;
-    segundos=minutosIntermedio%60;
+    horas=valor/3600;   //since 1 hour = 3600 seconds => (total seconds)/3600
+    minutosIntermedio=valor%3600;   // % is basically what's left of a division that doesn't return a float. Example: 5 % 2 = 1 (2 fits into 5 twice, but the 1 is left out)
+    minutos=minutosIntermedio/60;   // Whatever is left from the above operation is divided into the total amount of seconds in a minute
+    segundos=minutosIntermedio%60;  // And lastly, since we don't calculate miliseconds here, this operation is to find out of many seconds in a minute are still left
 
-    printf("O tempo convertido e %i Horas %i Minutos e %i Segundos. \n", horas, minutos, segundos);
+    printf("O tempo convertido e %i Horas %i Minutos e %i Segundos. \n", horas, minutos, segundos); //Prints the vars to screen
 }
