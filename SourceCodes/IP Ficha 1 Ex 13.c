44b07aa2ae7b7a9bf9db76c69db3e8692728084d
Gothicdread <35009972+Gothicdread@users.noreply.github.com>
2018-10-04 17:51:05 +0100

    Update IP Ficha 1 Ex 13.c

--- SourceCodes/IP Ficha 1 Ex 13.c
+++ SourceCodes/IP Ficha 1 Ex 13.c
@@ -5,21 +5,21 @@ void main()
     int numero1, numero2, numero3;
 
     printf("Numero 1: \n");
-    scanf("%i", &numero1);
+    scanf("%i", &numero1);  //scans input into all 3 int variables
     printf("Numero 2: \n");
     scanf("%i", &numero2);
     printf("Numero 3: \n");
     scanf("%i", &numero3);
 
-    if (numero1 > numero2 && numero1 > numero3)
+    if (numero1 > numero2 && numero1 > numero3) // IF CONDITION: tests if "numero1" is the largest number...
     {
-        printf("O maior numero e o numero %i", numero1);
+        printf("O maior numero e o numero %i", numero1);    //... if it prints it out of screen accordingly
     }
-    else if (numero2 > numero1 && numero2 > numero3)
+    else if (numero2 > numero1 && numero2 > numero3)    // Now it tests if "numero2" is the largest number...
     {
-        printf("O maior numero e o numero %i", numero2);
+        printf("O maior numero e o numero %i", numero2);    //... and if it is, prints it out on the screen
     }
-    else
+    else    //Doesn't need to test anything here, because if it's not 1 or 2, than it has to be 3 (ALTHOUGH I'M NOT VERIFYING IF ALL NUMBERS ARE EQUAL, PLEASE DON'T TEST OUT THAT MAJOR FLAW IN MY CODE PLEZ!!!!!)
     {
         printf("O maior numero e o numero %i", numero3);
     }
