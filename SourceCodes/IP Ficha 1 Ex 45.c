0e9c0e0233a67642549349a4449e0b83973a5bb4
Gothicdread <35009972+Gothicdread@users.noreply.github.com>
2018-10-04 23:38:54 +0100

    Update IP Ficha 1 Ex 45.c

--- SourceCodes/IP Ficha 1 Ex 45.c
+++ SourceCodes/IP Ficha 1 Ex 45.c
@@ -1,22 +1,28 @@
+/*
+
+This algorithm checks if the input number is prime or not. (fun...)
+
+*/
+
 #include <stdio.h>
 
-int divisores(int n)
-{
+int divisores(int n)    //A int function that will return an int.
+{   //The parameter is int n, the number that will be given to this function upon being called and the number that is being tested if it's prime or not
     int i, div, resto;
 
-    div=0;
+    div=0; //Our counter for divisions. This is what will determine if n is prime or not.
 
-    for(i=1;i<=n;i++)
+    for(i=1;i<=n;i++)   //i=1; while i doesn't reach n; increment i
     {
-        resto=n%i;
+        resto=n%i;  //Ah module! My nemesis...
 
-        if(resto==0)
+        if(resto==0)    //If module returns a 0, that means the division didn't produce a decimal number...
         {
-            div++;
+            div++;  //... meaning that whatever i is it can divide with n, which means this counter gets incremented
         }
     }
 
-    return div;
+    return div; //returns how many times n was successfully divided
 }
 
 void main()
@@ -25,15 +31,15 @@ void main()
 
     do
     {
-        printf("Introduza um numero: ");
+        printf("Introduza um numero: ");    //Gets the number to test
         scanf("%i", &num);
 
-        div=divisores(num);
+        div=divisores(num); //sends it to the function above and them div is equal to whatever it returns
 
-        if(div==2)
+        if(div==2)  //if the number was only successful divided twice (with 1 and with itself)...
         {
-            printf("NUMERO PRIMO! \n");
+            printf("NUMERO PRIMO! \n"); //... that means the number is prime.
         }
     }
-    while(num>0);
+    while(num>0);   //keeps repeating this operation until user inputs 0 or a negative number. (SEE!? No more of that main() inside main() bullshit anymore)
 }
