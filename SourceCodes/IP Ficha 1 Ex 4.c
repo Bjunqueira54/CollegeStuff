eb385a718ee664145e3a289a399e07ea1725a25f
Gothicdread <35009972+Gothicdread@users.noreply.github.com>
2018-10-04 23:22:14 +0100

    Update IP Ficha 1 Ex 4.c

--- SourceCodes/IP Ficha 1 Ex 4.c
+++ SourceCodes/IP Ficha 1 Ex 4.c
@@ -1,24 +1,30 @@
+/*
+
+Algorithm that calculates the average of 2 exams.
+
+*/
+
 #include <stdio.h>
 
 void main()
 {
-    float teste1,teste2,exame; //dados de entrada
-    float nota1,nota2,notaexame,notafinal; //Resultados Pretendidos
+    float teste1,teste2,exame; //dados de entrada. Input Data
+    float nota1,nota2,notaexame,notafinal; //Resultados Pretendidos. Output Data
 
-    printf("Nota do teste 1: ");
+    printf("Nota do teste 1: ");    //Asks for test 1
     scanf("%f",&teste1);
 
-    printf("Nota do teste 2: ");
+    printf("Nota do teste 2: ");    //Asks for test 2
     scanf("%f",&teste2);
 
-    printf("Nota do exame: ");
+    printf("Nota do exame: ");  //Asks for final exam
     scanf("%f",&exame);
 
-    nota1=teste1*0.1;
+    nota1=teste1*0.1;   //Tests are graded 10% each of the final grade
     nota2=teste2*0.1;
-    notaexame=exame*0.8;
-    notafinal=nota1+nota2+notaexame;
+    notaexame=exame*0.8;    //Exam is 80% of the final grade
+    notafinal=nota1+nota2+notaexame;    //Final grade is the addition of all tests and the exam (No shit Sherlock! ಠ_ಠ)
 
-    printf("O teste 1 vale %1.2f valores, o teste 2 vale %1.2f valores e o exame vale %2.2f valores \n",nota1,nota2,notaexame);
-    printf("A nota final da cadeira e %2.2f valores.",notafinal);
+    printf("O teste 1 vale %1.2f valores, o teste 2 vale %1.2f valores e o exame vale %2.2f valores \n",nota1,nota2,notaexame); //Prints each individual test and exam after percentage calculations
+    printf("A nota final da cadeira e %2.2f valores.",notafinal);   //Prints the final grade.
 }
