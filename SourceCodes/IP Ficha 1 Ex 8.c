1d65061a654150689b077e1e8b0328de66f88411
Gothicdread <35009972+Gothicdread@users.noreply.github.com>
2018-10-04 23:06:22 +0100

    Update IP Ficha 1 Ex 8.c

--- SourceCodes/IP Ficha 1 Ex 8.c
+++ SourceCodes/IP Ficha 1 Ex 8.c
@@ -1,19 +1,26 @@
+/*
+
+This simple code simply calculates job pay with taxes and without taxes.
+
+*/
+
 #include <stdio.h>
-#define TAXA_IRS 0.25
+
+#define TAXA_IRS 0.25   //Our defines :)
 #define TAXA_SS 0.11
 
 void main()
 {
-    float ordenadoIliquido, ordenadoLiquido, valorSS, valorIRS;
+    float ordenadoIliquido, ordenadoLiquido, valorSS, valorIRS; //Our float variables. We'll be using multiplications and percentage operations
 
-    printf("Valor do ordenado Iliquido: ");
-    scanf("%f",&ordenadoIliquido);
+    printf("Valor do ordenado Iliquido: "); //Gets users job pay without taxes first
+    scanf("%f", &ordenadoIliquido);
 
-    valorSS=ordenadoIliquido*TAXA_SS;
+    valorSS=ordenadoIliquido*TAXA_SS;   //Calculates SS and IRS tax
     valorIRS=ordenadoIliquido*TAXA_IRS;
-    ordenadoLiquido=ordenadoIliquido-valorSS-valorIRS;
+    ordenadoLiquido=ordenadoIliquido-valorSS-valorIRS; //User's end payment is original payment - both taxes
 
     printf("O seu ordenado liquido e %5.2f Euros,\n"
-           "descontou %5.2f Euros para a Segurança Social e \n"
-           "descontou %5.2f Euros para o IRS.",ordenadoLiquido, valorSS, valorIRS);
+           "descontou %5.2f Euros para a SeguranÃ§a Social e \n"
+           "descontou %5.2f Euros para o IRS.",ordenadoLiquido, valorSS, valorIRS); //Prints his liquid payment and how much taxes he had to pay for what type
 }
