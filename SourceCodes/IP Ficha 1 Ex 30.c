bd606ac1caa1ba4715c007a973941b1802f7e5cc
Gothicdread <35009972+Gothicdread@users.noreply.github.com>
2018-10-04 22:50:51 +0100

    Update IP Ficha 1 Ex 30.c

--- SourceCodes/IP Ficha 1 Ex 30.c
+++ SourceCodes/IP Ficha 1 Ex 30.c
@@ -1,22 +1,29 @@
+/*
+
+This code is used to calculate after how many years Population A grows larger than Pop B
+
+*/
+
 #include <stdio.h>
-#define TAXA_A 1.052
+
+#define TAXA_A 1.052    //These defines are growth rate. 1.052 = 5.2%
 #define TAXA_B 1.018
 
 void main()
 {
     int popA, popB, anos;
 
-    popA=500;
+    popA=500;   //Innitial values for PopA\B and years
     popB=1500;
     anos=0;
 
-    do
+    do  //CICLES! Keep repeating the following instructions...
     {
         popA=popA*TAXA_A;
         popB=popB*TAXA_B;
-        anos=anos+1;
+        anos=anos+1; //this could be replaced with "anos++;" but I was yound and stupid back then ¯\_(ツ)_/¯
     }
-    while(popA <= popB);
+    while(popA <= popB);    //... until Pop A is larger than Pop B
 
-    printf("A Populacao A ultrapassa a populacao B ao fim de %i anos.", anos);
+    printf("A Populacao A ultrapassa a populacao B ao fim de %i anos.", anos); //Prints years to terminal
 }
