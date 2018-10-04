f7ec0fa83c038aca93ba84afe80b0a8a7624ea15
Gothicdread <35009972+Gothicdread@users.noreply.github.com>
2018-10-04 22:35:43 +0100

    Update IP Ficha 1 Ex 16.c

--- SourceCodes/IP Ficha 1 Ex 16.c
+++ SourceCodes/IP Ficha 1 Ex 16.c
@@ -1,18 +1,24 @@
+/*
+
+In this code I created the most basic calculator of them all.
+
+*/
+
 #include <stdio.h>
 
-void adicao(float n1, float n2)
+void adicao(float n1, float n2) //simple function that takes 2 float parameters...
 {
-    float resultado;
-    resultado=n1+n2;
+    float resultado;    //... creates a result var
+    resultado=n1+n2;    //... and adds them
     printf("O resultado e %5.2f \n",resultado);
 }
-void subtracao(float n1,float n2)
+void subtracao(float n1,float n2)   //simple function that takes 2 float parameters...
 {
-    float resultado;
-    resultado=n1-n2;
+    float resultado;    //... creates a result var
+    resultado=n1-n2;    //... and subtracts them
     printf("O resultado e %5.2f \n",resultado);
 }
-void multi(float n1, float n2)
+void multi(float n1, float n2)  //You get the point...
 {
     float resultado;
     resultado=n1*n2;
@@ -27,24 +33,24 @@ void divisao(float n1, float n2)
 
 void main()
 {
-    float n1,n2;
+    float n1,n2;    //2 floats for all operations
     int tipo;
 
-    printf("Introduza um numero: ");
+    printf("Introduza um numero: ");    //Gets numbers from user
     scanf("%f",&n1);
 
     printf("Introduza outro numero: ");
     scanf("%f",&n2);
 
-    printf("Escolha o tipo de operacao: \n"
+    printf("Escolha o tipo de operacao: \n"     //Basic Menu
             "1 - Adicao \n"
             "2 - Subtracao \n"
             "3 - Multiplicacao \n"
             "4 - Divisao \n"
             "Tipo: ");
-    scanf("%i",&tipo);
+    scanf("%i",&tipo);  //Gets user's choice
 
-    switch(tipo)
+    switch(tipo)    //A switch(){} can replace "if\else" conditions. Usage: switch(var){ case VAR.VALUE: Instructions; break}
     {
         case 1: adicao(n1,n2); break;
         case 2: subtracao(n1,n2); break;
