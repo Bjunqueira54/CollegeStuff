4a17dd9d2c5ee616e390d9bffb1b4220f053c7a0
Gothicdread <35009972+Gothicdread@users.noreply.github.com>
2018-10-04 22:47:47 +0100

    Update IP Ficha 1 Ex 3.c

--- SourceCodes/IP Ficha 1 Ex 3.c
+++ SourceCodes/IP Ficha 1 Ex 3.c
@@ -1,16 +1,23 @@
+/*
+
+This code serves to calculate the perimeter and the area of a circle
+
+*/
+
 #include <stdio.h>
+
 #define PI 3.14
 
 void main()
 {
-    float raio, perimetro, area;
+    float raio, perimetro, area;    //All float variables
 
-    printf("Introduza o comprimento do raio do circulo: ");
+    printf("Introduza o comprimento do raio do circulo: "); //gets half of the diameter of the circle. (I forgot the word, ok? :c)
     scanf("%f",&raio);
 
-    perimetro=2*PI*raio;
-    area=PI*raio*raio;
+    perimetro=2*PI*raio;    //Circle Perimeter formula
+    area=PI*raio*raio;      //Circle Area formula
 
-    printf("O perimetro do circulo e %5.2f e a area e %5.2f \n",perimetro,area);
-    main();
+    printf("O perimetro do circulo e %5.2f e a area e %5.2f \n", perimetro, area);    //prints results to terminal
+    main(); //Again this recursive shit.... don't do this, please!
 }
