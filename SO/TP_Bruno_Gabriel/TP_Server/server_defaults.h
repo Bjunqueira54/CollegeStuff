05b062bc794525a7e34b64a99c949e6f1db701bc
Gothicdread <35009972+Gothicdread@users.noreply.github.com>
2018-10-28 15:01:49 +0000

    Add files via upload

---
+++ SO/TP_Bruno_Gabriel/TP_Server/server_defaults.h
@@ -0,0 +1,23 @@
+#ifndef SERVER_DEFAULTS_H
+#define SERVER_DEFAULTS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+#include "server.h"
+
+#define DEFAULT_DB_FILE "medit.db"
+    
+#define MEDIT_MAXLINES 15
+#define MEDIT_MAXCOLUMNS 45
+    
+#define MEDIT_TIMEOUT 10
+#define MEDIT_MAXUSERS 3
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
