bc3bc6f1f3aff49d6992b5fd7f708ce20a9fa1a0
the_dark_knight <the_dark_knight@TheDarKnight>
2018-11-07 17:01:25 +0000

    Merge origin/master
    Conflicts:
    	C++/POO_TP_Bruno_Gabriel/main.cpp

--- C++/POO_TP_Bruno_Gabriel/main.cpp
+++ C++/POO_TP_Bruno_Gabriel/main.cpp
@@ -6,9 +6,13 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    //drawUI(20,40); //Pending nCurses implementation
-    cout << "\nBem vindo a Cid Martin's Pirates" << endl;  // alterei o nome de proposito
-    cout << "(Pressione qualquer botao para continuar)" << endl;
+    initscr();
+    noecho();
+    //keypad(stdscr, TRUE);
+    //drawUI(20,40);
+    
+    mvwaddstr(stdscr, 9, 25, "Bem vindo a Cid Martin's Pirates");
+    mvwaddstr(stdscr, 11, 20, "(Pressione qualquer botao para continuar)");
     getch();
     clear();
     
@@ -16,12 +20,12 @@ int main(int argc, char** argv)
     while(name == "")
     {
         clear();
-        cout << "\nIntroduza o seu nome: ";
+        mvwaddstr(stdscr, 1, 1, "Introduza o seu nome: ");
         getline(cin, name);
         if(name == "") 
         {
             clear();
-            cout << "\nNome invalido!\n";
+            mvwaddstr(stdscr, 1, 1, "Nome invalido!");
             getch();
         }
     }
@@ -32,8 +36,9 @@ int main(int argc, char** argv)
     Player player(name);
     
     clear();
-    cout << "\nBoa sorte marinheiro " << name << "!" << endl;
-    cout << "(Pressiona qualquer botao para continuar)" << endl;
+    mvwaddstr(stdscr, 9, 28, "Boa sorte marinheiro ");
+    //mvwaddstr(stdscr, 9, 40, name);
+    mvwaddstr(stdscr, 11, 20, "(Pressione qualquer botao para continuar)");
     getch();
     clear();
     
@@ -72,6 +77,7 @@ int main(int argc, char** argv)
             //Phase 6: Enemy AI Ship spawn
         }
     }
+    endwin();
     return 0;
 }
 
