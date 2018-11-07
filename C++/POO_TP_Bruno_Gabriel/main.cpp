fb6c90ad091459249f0822e7590c1131fb7032c8
the_dark_knight <the_dark_knight@TheDarKnight>
2018-11-07 10:14:12 +0000

    !!

--- C++/POO_TP_Bruno_Gabriel/main.cpp
+++ C++/POO_TP_Bruno_Gabriel/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(int argc, char** argv)
 {
     //drawUI(20,40); //Pending nCurses implementation
-    cout << "\nBem vindo a Cid Martin's Pirates!" << endl;  // alterei o nome de proposito
+    cout << "\nBem vindo a Cid Martin's Pirates!!" << endl;  // alterei o nome de proposito
     cout << "(Pressiona qualquer botao para continuar)" << endl;
     
     string name;
