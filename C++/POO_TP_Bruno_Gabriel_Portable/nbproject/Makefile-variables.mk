d976738e218ded8d14782b8f36b625c35971e8fc
GothicDread <dreadlock54@hotmail.com>
2018-11-09 19:41:59 +0000

    09/11/2018 19:40
    -POO_TP_Bruno_Gabriel_Portable is now fully functional between Windows
    	and Linux (Mint 19.0, didn't test others) Operating Systems.
    -Function setScreenSize(int lines, int columns) is now fully functional
    	between Windows 10 and Linux Mint 19.0 (didn't test others, sorry)
    
    09/11/2018 19:30
    
    -Ported "Portability Project" to a copy of POO_TP_Bruno_Gabriel for
    	further testing named POO_TP_Bruno_Gabriel_Portable
    -Don't work on POO_TP_Bruno_Gabriel (the original project) until the 
    	portable version has been fully tested and deemed worthy

--- C++/POO_TP_Bruno_Gabriel_Portable/nbproject/Makefile-variables.mk
+++ C++/POO_TP_Bruno_Gabriel_Portable/nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
 CND_ARTIFACT_NAME_Debug=poo_tp_bruno_gabriel_portable
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/poo_tp_bruno_gabriel_portable
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/poo_tp_bruno_gabriel_portable
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
 CND_PACKAGE_NAME_Debug=pootpbrunogabrielportable.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/pootpbrunogabrielportable.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/pootpbrunogabrielportable.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
