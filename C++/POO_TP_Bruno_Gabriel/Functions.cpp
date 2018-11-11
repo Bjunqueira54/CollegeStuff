68425e0954a3f8bc24df2f7657f642abd55496c5
GothicDread <dreadlock54@hotmail.com>
2018-11-11 02:48:13 +0000

    10/11/2018	INPUT TIME BEFORE PUSHING
    -Fixed screen not clear from the Main Menu into Name Input screen;
    -Created new .cpp file for Main Game Loop;
    -Main Game now exits into Main Menu after command "sair"\"exit";
    -Enforced the 20 characters limit on the name input;
    -Fixed Main Menu string formating;

--- C++/POO_TP_Bruno_Gabriel/Functions.cpp
+++ C++/POO_TP_Bruno_Gabriel/Functions.cpp
@@ -3,13 +3,31 @@
 
 using namespace std;
 
+//Draws a neat screen wide box for the main menu.
+//Don't forget to give it the a Language object
+//or else nothing will show up
 void drawMainMenu(const Language lang)
 {
     clear();
-    mvwaddstr(stdscr, 7, getCenter(lang.getLine(2)), lang.getLine(2));
-    mvwaddstr(stdscr, 9, getCenter(lang.getLine(3)), lang.getLine(3));
-    mvwaddstr(stdscr, 11, getCenter(lang.getLine(4)), lang.getLine(4));
-    mvwaddstr(stdscr, 13, getCenter(lang.getLine(5)), lang.getLine(5));
+    for(int y = 0; y<getmaxy(stdscr); y++)
+    {
+        for(int x=0; x<getmaxx(stdscr); x++)
+        {
+            if(y==0 || y==(getmaxy(stdscr) - 1))
+                mvwaddch(stdscr, y, x, '*');
+            else
+            {
+                if(x==0 || x==(getmaxx(stdscr) - 1))
+                    mvwaddch(stdscr, y, x, '*');
+                else
+                    mvwaddch(stdscr, y, x, ' ');
+            }
+        }
+    }
+    mvwaddstr(stdscr, 5, getCenter(lang.getLine(2)), lang.getLine(2));
+    mvwaddstr(stdscr, 5, getCenter(lang.getLine(3)), lang.getLine(3));
+    mvwaddstr(stdscr, 5, getCenter(lang.getLine(4)), lang.getLine(4));
+    mvwaddstr(stdscr, 5, getCenter(lang.getLine(5)), lang.getLine(5));
     refresh();
 }
 
@@ -62,6 +80,24 @@ int getCenter(string a)
     x = (getmaxx(stdscr) - strlen(a.c_str()))/2;
     
     return x;
+}
+
+int getVertCenter(WINDOW *win, int n, int *esp)
+{
+    int y, r;
+    
+    y = getmaxy(win);
+    
+    *esp = (y-(n+2))/(n+1); //rounds down
+    
+    r = (y-(n+2))%(n+1);
+    
+    if(r%2 == 0)
+    {}
+    else
+    {}
+    
+    return r;
 }
 
 void tutorial()
