d2cedce18804b613f9a0d8ea4464f40281f6222e
GothicDread <dreadlock54@hotmail.com>
2018-11-17 22:22:31 +0000

    17/11/2018 22:20
    -Fixed the Test_Cursor example;

--- C++/Test_Cursor/main.cpp
+++ C++/Test_Cursor/main.cpp
@@ -10,10 +10,12 @@ using namespace std;
 int main(int argc, char** argv)
 {
     initscr();
+    keypad(stdscr, TRUE);
     noecho();
     
-    char opt;
+    int opt;
     int y=0, x=0, maxy, maxx;
+    int value;
     
     getmaxyx(stdscr, maxy, maxx);
     maxy--;
@@ -22,31 +24,45 @@ int main(int argc, char** argv)
     move(y,x);
     refresh();
     
+    /*while(1)
+    {
+        ostringstream con;
+        int value;
+        
+        opt = getch();
+        
+        value = opt;
+        con << value;
+        mvwaddstr(stdscr, 1, 1, con.str().c_str());
+        refresh();
+        clear();
+    }*/
+    
     do
     {
         opt = getch();
         
         switch(opt)
         {
-            case 65:
+            case KEY_UP:
                 if(y>0)
                     y--;
                 else if(y==0)
                     y=maxy;
                 break;
-            case 66:
+            case KEY_DOWN:
                 if(y<maxy)
                     y++;
                 else if(y==maxy)
                     y=0;
                 break;
-            case 67:
+            case KEY_RIGHT:
                 if(x<maxx)
                     x++;
                 else if(x==maxx)
                     x=0;
                 break;
-            case 68:
+            case KEY_LEFT:
                 if(x>0)
                     x--;
                 else if(x==0)
