fef5702741ac39694f1818b72621284a3fcca42c
GothicDread <dreadlock54@hotmail.com>
2018-11-19 09:05:33 +0000

    19/11/2018 09:05
    -Fixed a bug with parseCmd() "config" command:
    	-Fixed file reading to Settings Class.

--- C++/POO_TP_Bruno_Gabriel/Functions.h
+++ C++/POO_TP_Bruno_Gabriel/Functions.h
@@ -6,7 +6,7 @@
 #include "Map.h"
 
 #define GAME_VERSION_MAJOR "0"
-#define GAME_VERSION_MINOR "06"
+#define GAME_VERSION_MINOR "07"
 
 #define MAP_MAXY 40     //Merchants and Pirates Max Height
 #define MAP_MAXX 120    //Merchants and Pirates Max Width
