cb7a57bd477805c802b77a58d60f0bd3b131d615
GothicDread <dreadlock54@hotmail.com>
2018-11-28 10:20:10 +0000

    medit_defaults.h

---
+++ SO/TP_Bruno_Gabriel/medit_defaults.h
@@ -0,0 +1,25 @@
+#ifndef SERVER_DEFAULTS_H
+#define SERVER_DEFAULTS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DEFAULT_DB_FILE "medit.db"
+    
+#define MEDIT_MAXLINES 15
+#define MEDIT_MAXCOLUMNS 45
+    
+#define MEDIT_TIMEOUT 10
+#define MEDIT_MAXUSERS 3
+
+#define MAXNAME 8
+
+#define MEDIT_DEFAULT_MAIN_PIPE "~/medit_main_pipe"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
