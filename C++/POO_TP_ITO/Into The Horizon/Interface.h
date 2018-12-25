61534def93c1ba7d5e9cbd16ec2e3926257cc634
the_dark_knight <the_dark_knight@TheDarKnight>
2018-12-25 21:04:22 +0000

    INTO THE (H)ORIZON

--- C++/POO_TP_ITO/Into The Horizon/Interface.h
+++ C++/POO_TP_ITO/Into The Horizon/Interface.h
@@ -3,17 +3,31 @@
 
 #include "headers.h"
 
+#define MAX_X 120
+#define MAX_Y 40
+
 class Interface
 {
+    static vector <string> cmd;
+    static vector <string> line;
+    static vector <string> tutorial;
     
 public:
     
-    Interface();
+    Interface(char lang);
     Interface(const Interface& orig);
     
-    void SetScreenSize(int lines, int columns);
+    const char* getCmd(int i);
+    const char* getLine(int i);
+    static void tutPage();
+    static void credPage();
     
     virtual ~Interface();
 };
+
+int getCenter(const char* a);
+int getCenter(string a);
+int getVertCenter(WINDOW *win, int n, int &r, int &flag);
+void SetScreenSize(int lines, int columns);
 
 #endif
