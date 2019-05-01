7bfc6cd06b49cd109802872bdb2c5a94e482563b
GothicDread <dreadlock54@hotmail.com>
2019-05-01 14:06:56 +0100

    idek

--- SO2/TP_SO2/TP_SO2_DLL/TP_SO2_DLL.cpp
+++ SO2/TP_SO2/TP_SO2_DLL/TP_SO2_DLL.cpp
@@ -2,4 +2,13 @@
 //
 
 #include "stdafx.h"
+#include "Vars.h"
 
+//eu não sei fazer esta merda
+DWORD WINAPI LogIn(wchar_t *username)
+{
+	if (wcscmp(PlayerUsername, L""))
+		PlayerUsername = username;
+	else
+		LogIn(username);
+}
---
+++ SO2/TP_SO2/TP_SO2_DLL/Vars.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	wchar_t *PlayerUsername;
+	
+
+#ifdef __cplusplus
+}
+#endif
