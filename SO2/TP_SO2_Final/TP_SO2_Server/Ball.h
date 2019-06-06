f1f58b71b0dd54bdf1693a63fcf0acdc69259033
GothicDread <dreadlock54@hotmail.com>
2019-06-06 14:15:58 +0100

    more idk

---
+++ SO2/TP_SO2_Final/TP_SO2_Server/Ball.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "includes.h"
+
+typedef struct ball
+{
+	float x, y;
+	int speed; //time in miliseconds for position change = 1/speed;
+
+
+} Ball, *pBall;
---
+++ SO2/TP_SO2_Final/TP_SO2_Server/defines.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define GAMEAREA_MAX_X 40
+#define GAMEAREA_MAX_Y 20
+
+#define GAMEAREA_MIN_X 0
+#define GAMEAREA_MIN_Y 0
